94ce94617f511fea55c30313cbad562a95479fb3
t-bast <bastien@stratumn.com>
2018-01-31 11:56:57 +0100

    Create docker images for the demo

--- proof-of-location/prove.sh
+++ proof-of-location/prove.sh
@@ -10,7 +10,17 @@ read X
 echo Y=?
 read Y
 
-echo "#!/bin/sh\necho $X\necho $Y" > bin/exo0
+echo "#!/bin/sh" > bin/exo0
+echo "echo $X" >> bin/exo0
+echo "echo $Y" >> bin/exo0
 chmod +x bin/exo0
 
 bin/pepper_prover_proof_of_location prove proof_of_location.pkey proof_of_location.inputs proof_of_location.outputs proof_of_location.proof
+
+# Output the content of the outputs file.
+echo "* Computation output:"
+cat prover_verifier_shared/proof_of_location.outputs
+
+# Output the proof bytes.
+echo "* Proof hex bytes:"
+hexdump -e '16/1 "%02x " "\n"' prover_verifier_shared/proof_of_location.proof
